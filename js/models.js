d3ccf906aaf5ea84c77229e1f9d2bbc8dbe70e1c
Racha777 <jramoschang@gmail.com>
2022-05-07 11:59:54 -0500

    helper js

--- js/models.js
+++ js/models.js
@@ -1,12 +1,25 @@
+import {getColorsData} from "../helpers/helper.js";
+
 export const models=()=>{
+    // Chart.defaults.color='#000';
     const data={
         labels:['javascript','HTML','CSS'],
         datasets:[{
             data:[60,50,50],
+            borderColor:getColorsData(),
+            backgroundColor:getColorsData(40)
         }]
     };
 
-    new Chart('dashboardModelsChart',{type: 'doughnut', data});
+    const options={
+        plugins:{
+            legend:{
+                position: 'bottom'
+            },
+        },
+    };
+
+    new Chart('dashboardModelsChart',{type: 'doughnut', data, options});
 }
 
 export default models;
